8bd895a02ed07d7bba6d210ded1645eef7f26c80
Madiyar Talgatuly <133092635+Madi4ar@users.noreply.github.com>
2023-10-09 18:03:04 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -82,7 +82,21 @@ $(document).ready(function(){
                   ]
       });
 
-
+      $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
     
 
     $('.mod').click(function() {
